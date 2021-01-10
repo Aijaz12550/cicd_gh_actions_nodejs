9b95e8fa0f43315f93a061a603a2e398dacb0989
Aijaz Abbasi <muhammadaijaz76@gmail.com>
2021-01-11 00:49:23 +0500

    title changes 2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'GitHub Actions HI! 2222222' });
+  res.render('index', { title: 'GitHub Actions HI! 2222222 test 2' });
 });
 
 module.exports = router;
